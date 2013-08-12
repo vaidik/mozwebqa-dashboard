d513be4ad21825472319fa69768ecbd1fa8bc4e4
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-08-12 16:50:53 -0700

    commit to gh-pages only

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,9 @@ from datetime import datetime
 from fabric.api import lcd
 from fabric.api import local
 from fabric.api import run
+from fabric.api import settings
 
+DUMPS_DIR = 'dumps'
 WORKSPACE_DIR = 'workspace'
 
 repos = [
@@ -13,8 +15,11 @@ repos = [
 
 
 def prepare_deploy():
-    if not os.path.exists('workspace'):
-        local('mkdir workspace')
+    if not os.path.exists(WORKSPACE_DIR):
+        local('mkdir %s' % WORKSPACE_DIR)
+
+    if not os.path.exists(os.path.join(WORKSPACE_DIR, DUMPS_DIR)):
+        local('mkdir %s' % os.path.join(WORKSPACE_DIR, DUMPS_DIR))
 
     for repo in repos:
         name = repo.split('/')[-1].split('.')[0]
@@ -25,6 +30,12 @@ def prepare_deploy():
                     local('git pull')
             else:
                 local('git clone %s' % repo)
-        local('python testparser.py %s > dumps/%s.json' % (path, name))
-    local("git add dumps/* && git commit -m \'dump on %s\'" % datetime.now().strftime("%B %d, %Y at %H:%M:%S"))
-    local("git push")
+        local('python testparser.py %s > %s.json' % (path,
+                                                     os.path.join(WORKSPACE_DIR, DUMPS_DIR, name)))
+
+    local("git checkout gh-pages")
+    with settings(warn_only=True):
+        local("cp -rvf %s %s" % (os.path.join(WORKSPACE_DIR, DUMPS_DIR), DUMPS_DIR))
+        local("git add dumps/* && git commit -m \'dump on %s\'" % datetime.now().strftime("%B %d, %Y at %H:%M:%S"))
+        local("git checkout master")
+    local("git push origin gh-pages")
