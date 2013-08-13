1733fdc32767172a20a7aee636caa1aab979810f
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-08-13 16:51:58 -0700

    frontend is up

---
+++ js/testanalysis.js
@@ -0,0 +1,18 @@
+app.controller('testanalysis', function($scope, $http) {
+    $scope.parse_data = {};
+
+    $scope.init = function() {
+        $http.get('/config.json').success(function(data) {
+            for (var repo in data.repos) {
+                repo = data.repos[repo].split('/');
+                repo = repo[repo.length - 1];
+                
+                (function(repo_name) {
+                    $http.get('/dumps/' + repo_name + '.json').success(function(data) {
+                        $scope.parse_data[repo_name] = data;
+                    });
+                }) (repo);
+            }
+        });
+    }
+});
